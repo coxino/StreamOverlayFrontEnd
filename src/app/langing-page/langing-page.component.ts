6bbdca4ea66dc26dece94c7258613f24fb2df8ee
coxino <coxinotv@gmail.com>
2023-01-21 17:18:06 +0200

    updates, tooltips, anouncements
    finish landing page before release

--- src/app/langing-page/langing-page.component.ts
+++ src/app/langing-page/langing-page.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { CookieService } from 'ngx-cookie';
-import { Settings } from 'src/assets/database/Models/databaseStructure';
 import { SiteSettings } from 'src/assets/SiteSettings';
-import { IntervalRequestService } from 'src/services/interval-request.service';
 
 @Component({
   selector: 'app-langing-page',
@@ -11,7 +7,7 @@ import { IntervalRequestService } from 'src/services/interval-request.service';
   styleUrls: ['./langing-page.component.scss']
 })
 export class LangingPageComponent implements OnInit {
-  
+  version = SiteSettings.Version;
   title = SiteSettings.SiteName;
   
   constructor(){}
--- src/assets/SiteSettings.ts
+++ src/assets/SiteSettings.ts
@@ -1,5 +1,5 @@
 export class SiteSettings{
     public static readonly currency:string = "ron";
-    static Version: "0.88.91";
+    public static readonly Version:string = "0.99.91";
     public static readonly SiteName:string = "Casino Streaming Companion";
 }
